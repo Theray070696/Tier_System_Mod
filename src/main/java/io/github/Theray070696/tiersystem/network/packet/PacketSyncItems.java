36103e653a87486a3946e3e9c823e9c329e72c7e
Theray070696 <Theray070696@gmail.com>
2015-04-03 23:33:22 -0400

    Fixes #3

--- src/main/java/io/github/Theray070696/tiersystem/network/packet/PacketSyncItems.java
+++ src/main/java/io/github/Theray070696/tiersystem/network/packet/PacketSyncItems.java
@@ -3,6 +3,7 @@ package io.github.Theray070696.tiersystem.network.packet;
 import com.google.common.base.Joiner;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.ByteBufUtils;
+import io.github.Theray070696.tiersystem.lib.ModInfo;
 import io.github.Theray070696.tiersystem.tier.TierHandler;
 import io.github.Theray070696.tiersystem.util.ArrayUtilities;
 import io.netty.buffer.ByteBuf;
@@ -30,46 +31,49 @@ public class PacketSyncItems extends AbstractPacket
     @Override
     public void encodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
     {
-        Map<String, Integer> itemsTurnedIn;
-
-        if(TierHandler.getItemsTurnedInForPlayer(this.entityPlayer) != null)
+        if(ModInfo.devModeActive)
         {
-            itemsTurnedIn = TierHandler.getItemsTurnedInForPlayer(this.entityPlayer);
-        } else
-        {
-            itemsTurnedIn = null;
-        }
+            Map<String, Integer> itemsTurnedIn;
 
-        String[] itemsTurnedInString;
-        int[] amountTurnedInInt;
-
-        if(itemsTurnedIn != null && !itemsTurnedIn.isEmpty())
-        {
-            itemsTurnedInString = ArrayUtilities.objectArrayToStringArray(itemsTurnedIn.keySet().toArray());
-            amountTurnedInInt = ArrayUtilities.objectArrayToIntArray(itemsTurnedIn.values().toArray());
-        } else
-        {
-            itemsTurnedInString = null;
-            amountTurnedInInt = null;
-        }
-
-        if(itemsTurnedIn != null && itemsTurnedInString != null && amountTurnedInInt != null && itemsTurnedInString.length == amountTurnedInInt.length)
-        {
-            String[] items = new String[itemsTurnedInString.length];
-
-            for(int i = 0; i < itemsTurnedInString.length; i++)
+            if(TierHandler.getItemsTurnedInForPlayer(this.entityPlayer) != null)
             {
-                items[i] = itemsTurnedInString[i] + "%" + amountTurnedInInt[i];
+                itemsTurnedIn = TierHandler.getItemsTurnedInForPlayer(this.entityPlayer);
+            } else
+            {
+                itemsTurnedIn = null;
             }
 
-            ByteBufUtils.writeUTF8String(buffer, Joiner.on("/").skipNulls().join(items));
+            String[] itemsTurnedInString;
+            int[] amountTurnedInInt;
+
+            if(itemsTurnedIn != null && !itemsTurnedIn.isEmpty())
+            {
+                itemsTurnedInString = ArrayUtilities.objectArrayToStringArray(itemsTurnedIn.keySet().toArray());
+                amountTurnedInInt = ArrayUtilities.objectArrayToIntArray(itemsTurnedIn.values().toArray());
+            } else
+            {
+                itemsTurnedInString = null;
+                amountTurnedInInt = null;
+            }
+
+            if(itemsTurnedIn != null && itemsTurnedInString != null && amountTurnedInInt != null && itemsTurnedInString.length == amountTurnedInInt.length)
+            {
+                String[] items = new String[itemsTurnedInString.length];
+
+                for(int i = 0; i < itemsTurnedInString.length; i++)
+                {
+                    items[i] = itemsTurnedInString[i] + "%" + amountTurnedInInt[i];
+                }
+
+                ByteBufUtils.writeUTF8String(buffer, Joiner.on("/").skipNulls().join(items));
+            }
         }
     }
 
     @Override
     public void decodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
     {
-        if(FMLCommonHandler.instance().getSide().isClient())
+        if(FMLCommonHandler.instance().getSide().isClient() && ModInfo.devModeActive)
         {
             this.items = ByteBufUtils.readUTF8String(buffer).split("/");
         }
@@ -78,24 +82,27 @@ public class PacketSyncItems extends AbstractPacket
     @Override
     public void handleClientSide(EntityPlayer player)
     {
-        TierHandler.clearItemsTurnedInForPlayer(player);
-
-        for(int i = 0; i < this.items.length; i++)
+        if(ModInfo.devModeActive)
         {
-            String[] itemAndAmount = this.items[i].split("%", 2);
+            TierHandler.clearItemsTurnedInForPlayer(player);
 
-            String item = itemAndAmount[0];
-            int amount = Integer.parseInt(itemAndAmount[1]);
-            int metadata = 0;
-
-            if(item.contains(" "))
+            for(int i = 0; i < this.items.length; i++)
             {
-                String[] itemMetaAndAmount = item.split(" ", 2);
-                item = itemMetaAndAmount[0];
-                metadata = Integer.parseInt(itemMetaAndAmount[1]);
-            }
+                String[] itemAndAmount = this.items[i].split("%", 2);
 
-            TierHandler.addItemTurnedInForPlayer(player, item, metadata, amount);
+                String item = itemAndAmount[0];
+                int amount = Integer.parseInt(itemAndAmount[1]);
+                int metadata = 0;
+
+                if(item.contains(" "))
+                {
+                    String[] itemMetaAndAmount = item.split(" ", 2);
+                    item = itemMetaAndAmount[0];
+                    metadata = Integer.parseInt(itemMetaAndAmount[1]);
+                }
+
+                TierHandler.addItemTurnedInForPlayer(player, item, metadata, amount);
+            }
         }
     }
 
