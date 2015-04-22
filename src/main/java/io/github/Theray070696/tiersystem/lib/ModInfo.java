d6d7f4572243c2bbea7a34533c0d6d45b9bf2443
Theray070696 <Theray070696@gmail.com>
2015-04-22 09:01:43 -0400

    Updated build.gradle, now properly replaces version number in ModInfo.java

--- src/main/java/io/github/Theray070696/tiersystem/lib/ModInfo.java
+++ src/main/java/io/github/Theray070696/tiersystem/lib/ModInfo.java
@@ -9,7 +9,7 @@ public class ModInfo
     public static final String MOD_ID = "tiersystem";
     public static final String CHANNEL = MOD_ID;
     public static final String MOD_NAME = "Tier System";
-    public static final String MOD_VERSION = "${version}";
+    public static final String MOD_VERSION = "version";
     public static final String CLIENT_PROXY = "io.github.Theray070696.tiersystem.proxy.ClientProxy";
     public static final String SERVER_PROXY = "io.github.Theray070696.tiersystem.proxy.ServerProxy";
     public static final boolean devModeActive = false;
